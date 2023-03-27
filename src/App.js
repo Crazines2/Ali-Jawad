9db18774a83402f17c01bbc074f09f2393765b86
5sixten <128362928+5sixten@users.noreply.github.com>
2023-03-27 12:36:59 +0200

    hej

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React today
-        </a>
-      </header>
-    </div>
+
+<body>
+  <div class="app"></div>
+  <div class="app-logo"></div>
+  <div class="app-header"></div>
+  <div class="app-link"></div>
+  <div class="h1"></div>
+
+  <div class="hem"><h6>HEM</h6></div>
+  <div class="kalender"><h6>KALENDER</h6></div>
+  <div class="todo"><h6>TO DO</h6></div>
+
+  <div class="line1"></div>
+  <div class="line2"></div>
+  <div class="line3"></div>
+
+</body>
+
   );
 }
 
